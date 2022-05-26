55d8a3de260237665a1f5be9a6b758eb2d579fa5
Quentin Bullock <q@quentins-mbp.home>
2022-05-26 13:17:54 -0400

    utilize existing hibp component

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import sha1 from 'sha1'
 import { AutoForm as form1 } from 'uniforms-unstyled';
 import { AutoForm as form2 } from 'uniforms-antd';
 import { AutoForm as form3 } from 'uniforms-bootstrap3';
@@ -10,6 +11,8 @@ import { bridge as schema } from './schema/json-schema';
 import { images as images } from './images';
 import { taglines } from './PhonyTaglines';
 import { postData } from './HttpService';
+import { ConsoleSqlOutlined } from '@ant-design/icons';
+import { useEffect } from 'react';
 
 const templates = [
   [form1, null],
@@ -38,37 +41,61 @@ function addCss(fileName) {
 
 async function digestMessage(message) {
   const msgUint8 = new TextEncoder().encode(message);
-  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
+  const hashBuffer = await crypto.subtle.digest('SHA-1', msgUint8);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
   const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
   return hashHex;
 }
 
-async function checkPwned(submission) {
-  const digest = await digestMessage(submission.password.slice(0, 5))
-  const resp = await postData('/log', { username: submission.username, digest }).then(d=>d.json);
-  console.log(resp)
-  /*
-  if (callback != null) {
+async function checkPassword(password, setPwned) {
+  const hash = sha1(password).toUpperCase()
 
-    callback(await postData('/log', { username: submission.username, digest }))
-  }*/
+  const prefix = hash.substr(0, 5)
+  const suffix = hash.substr(5)
 
+  fetch(`https://api.pwnedpasswords.com/range/${prefix}`, {
+    headers: {
+      Accept: "application/vnd.haveibeenpwned.v2+json"
+    }
+  })
+    .then(resp => resp.ok ? resp.text() : Promise.reject(resp.text()))
+    .then(resp => resp.split('\n')
+      .map(entry => {
+        const [suffix, count] = entry.split(":")
+        return {
+          suffix,
+          count: parseInt(count, 10)
+        }
+      }))
+    .then(results => {
+      const result = results.some(result => result.suffix.toLowerCase() === suffix.toLowerCase())
+      console.log(result)
+      setPwned(result)
+    })
+    .catch(e => {
+      console.log(e)
+    })
 }
 
 export function App() {
-  const [submitted, setSubmitted] = React.useState(false)
+  const [submitted, setSubmitted] = React.useState(null)
   const [pwned, setPwned] = React.useState(false)
 
   const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
   })
 
+  useEffect(x=>{
+    if(submitted){
+      postData('/log', {submitted,pwned})
+    }
+  })
+
   const value = params.template;
   const template = templates[value != null ? parseInt(value) : Math.floor(Math.random() * templates.length)]
   addCss(template[1])
 
-  if (!submitted) {
+  if (submitted == null) {
     return (
       <div class="flex flex-auto justify-center relative rounded-xl overflow-auto p-8 w-full">
         <div class="relative rounded-xl overflow-auto p-8 w-6/12 justify-center items-center flex flex-col">
@@ -76,8 +103,9 @@ export function App() {
           <h3>{taglines[Math.floor(Math.random() * taglines.length)]}</h3>
           {render(template[0], {
             schema: schema, onSubmit: e => {
-              checkPwned(e, setPwned)
-              setSubmitted(true);
+              checkPassword(e.password, setPwned)
+              console.log(e)
+              setSubmitted(e.email);
             }
           })}
         </div>
@@ -89,7 +117,7 @@ export function App() {
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
           <div class="p-6 text-center">
             <svg class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Uh oh! It looks like you fell for a phishing attempt!</h3>
+            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Uh oh! It looks like you fell for a phishing attempt! {pwned && "\nand your password has been involved in a breach!"}</h3>
             <div class="h-14 bg-gradient-to-r from-cyan-500 to-blue-500"></div>
             <button data-modal-toggle="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
               Yes, I'm sure
--- HttpService.ts
+++ HttpService.ts
@@ -13,7 +13,7 @@ async function postData(url = '', data = {}) {
       referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       body: JSON.stringify(data) // body data type must match "Content-Type" header
     });
-    return response.json(); // parses JSON response into native JavaScript objects
+    return response.text(); // parses JSON response into native JavaScript objects
   }
 
 export {postData}
