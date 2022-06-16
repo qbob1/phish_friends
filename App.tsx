9856c9b204963819b851174cc3989682537e2621
Quentin Bullock <q@quentins-mbp.home>
2022-06-16 16:55:42 -0400

    Add video content to landing page

--- App.tsx
+++ App.tsx
@@ -108,13 +108,12 @@ export function App() {
       <div class="relative p-4 w-full h-full md:h-auto">
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
           <div class="p-6 text-center">
-            <svg class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Uh oh! It looks like you fell for a phishing attempt! {pwned && "\nand your password has been involved in a breach!"}</h3>
-            <div class="h-14 bg-gradient-to-r from-cyan-500 to-blue-500"></div>
-            <button data-modal-toggle="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-              Yes, I'm sure
-            </button>
+            <svg class="mx-auto mb-4 w-14 h-14 text-black " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            <h2 class="mb-5 text-lg font-normal text-black ">Uh oh! It looks like you fell for a phishing attempt! {pwned && "\nYour password has been involved in a breach!"}</h2>
+            
           </div>
+
+          <video controls className="w-full aspect-video" style={{'aspect-ratio': '12 / 6'}} src="https://phishing-content.us-southeast-1.linodeobjects.com/C3E10%20-%20NANO%20-%20A%20Terminal%20Mistake%20-%20Spear%20Phishing.mp4"/>
         </div>
       </div>
     </div>)
